0e3bedeef8ed9dd6726379b9067f71846a96908e
muniri92 <MunirIbrahim@Munirs-MacBook-Pro.local>
2016-01-21 01:03:29 -0800

    fixed up the code so that the chart is deleted and regenerated everytime the button is clicked. gave labels and titles to charts. still a few issue with the math, but i couldn't find the problem

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,6 @@
 'use strict'
-
 var productArray = [];
 var count = 0;
-
 // CONSTRUCTOR
 function Product(names, src) {
   this.names = names;
@@ -12,23 +10,18 @@ function Product(names, src) {
   this.percentClick = 0;
   productArray.push(this);
 }
-
 // METHOD FOR CALCULATING PERCENTAGE
 Product.prototype.percent = function () {
-  this.percentClick = ((this.timesClicked / this.timesDisplayed).toFixed(2) * 100);
+  this.percentClick = (+((this.timesClicked / this.timesDisplayed).toFixed(2)) * 100);
 };
-
-// GENERATE RANDOM NUMBER BTW 1-14
 function generateRandom() {
-  return +(Math.floor((Math.random() * 14)));
+  return +(Math.floor((Math.random() * productArray.length)));
 };
-
 // ALL PRODUCT ARRAY
 var allProducts = [new Product('bag', 'bag.jpg'), new Product('banana', 'banana.jpg'), new Product('boots', 'boots.jpg'), new Product('chair', 'chair.jpg'), new Product('cthulhu', 'cthulhu.jpg'), new Product('dragon', 'dragon.jpg'), new Product('pen', 'pen.jpg'), new Product('scissors', 'scissors.jpg'), new Product('shark', 'shark.jpg'), new Product('sweep', 'sweep.jpg'), new Product('unicorn', 'unicorn.jpg'), new Product('usb', 'usb.gif'), new Product('waterCan', 'water-can.jpg'), new Product('wineGlass', 'wine-glass.jpg')];
-
 // TO AVIOD REPEATING PICTURES
 var rand1, rand2, rand3;
-function randomImage() {
+function random() {
   var img1 = document.getElementById('firstImage');
   rand1 = generateRandom();
   img1.src = allProducts[rand1].src;
@@ -48,8 +41,7 @@ function randomImage() {
   img3.src = allProducts[rand3].src;
   allProducts[rand3].timesDisplayed++;
 }
-randomImage();
-
+random();
 // EVENT HANDLER FOR IMAGE
 function handleImage(image) {
   image.timesClicked += 1;
@@ -59,10 +51,9 @@ function handleImage(image) {
   dataSets1();
   dataSets2();
   dataSets3();
-  randomImage();
+  random();
 }
-
-// EVENT LISTENER FOR IMAGE
+// EVENT LISTENERS FOR IMAGE
 firstImage.addEventListener('click', function(){
   handleImage(allProducts[rand1]);
 });
@@ -70,99 +61,112 @@ secondImage.addEventListener('click', function() {
   handleImage(allProducts[rand2]);
 });
 thirdImage.addEventListener('click', function() {
-  handleImage(allProducts[rand1]);
+  handleImage(allProducts[rand3]);
 });
-
 // BUTTON FUNCTION
 var hidden;
 function checkButton() {
-  if (count < 3) {
-    // console.log('count is: ' + count);
+  if (count < 15) {
     results.removeAttribute(hidden);
   } else {
     results.style.display = 'block';
   }
 }
-
 // EVENT LISTENER FOR BUTTON
-var buttons = document.getElementById('results')
-buttons.addEventListener('click', handleButton);
-// HANDLER FOR BUTTON
 function handleButton() {
-  buttons.textContent = 'Display Updated Results';
   chart1();
   chart2();
-  chart3();
 }
-
+var button = document.getElementById('results')
+// HANDLER FOR BUTTON
+button.addEventListener('click', handleButton);
 // MAKE DATA ARRAY FOR PERCENT CHART
 var percentChart  = [];
-var label = ['Bag', 'Banana', 'Boots', 'Chair', 'Cthulhu', 'Dragon', 'Pen', 'Scissors', 'Shark', 'Sweep', 'Unicorn', 'USB', 'Water Can', 'Wine Glass'];
 function dataSets1() {
   for (var i = 0; i < allProducts.length; i++){
     percentChart[i] =  productArray[i].percentClick;
   }
-};
-
+}
 // MAKE PERCENT CHART
+var sectionTable = document.getElementById('sectionTable');
 function chart1() {
+  var nameChart1 = document.getElementById('nameChart1');
+  nameChart1.textContent = 'Percent That Item Was Clicked vs Displayed';
+  var title1 = document.getElementById("title1");
+  var canvasOne = document.getElementById("canvasOne");
+  var throwawayNode = title1.removeChild(canvasOne);
+  var canvasOne = document.createElement('canvas');
+  canvasOne.id = 'canvasOne';
+  canvasOne.setAttribute('width', '400');
+  canvasOne.setAttribute('height', '400');
+  title1.appendChild(canvasOne);
   var data = {
-    labels: label,
+    labels: ['Bag', 'Banana', 'Boots', 'Chair', 'Cthulhu', 'Dragon', 'Pen', 'Scissors', 'Shark', 'Sweep', 'Unicorn', 'USB', 'Water Can', 'Wine Glass'],
+    datasets: [
       {
-        label: 'Chart Name',
-        label: "My Second dataset",
-        fillColor: "rgba(202,205,206,0.5)",
-        strokeColor: "rgba(202,205,206,0.8)",
-        highlightFill: "rgba(202,205,206,0.75)",
-        highlightStroke: "rgba(202,205,206,1)",
+        label: 'Percent of Times Clicked vs Times Displayed',
+        fillColor: '#E6E6FA',
+        strokeColor: '#E6E6FA',
+        highlightFill: "#FFE4B5",
+        highlightStroke: "#FFE4B5",
+        scaleFontColor: "#DCDCDC",
         data: percentChart
       }
     ]
   };
-  // MAKE CHART
   var ctx = document.getElementById('canvasOne').getContext('2d');
   var myBarChart = new Chart(ctx).Bar(data);
+  document.getElementById('legendOne').innerHTML = myBarChart.generateLegend();
 }
-
-// MAKE DATA ARRAY FOR DISPLAYED ITEMS
+// MAKE DATA ARRAY FOR DISPLAYED / CLICKED ITEMS
 var displayedChart  = [];
 function dataSets2() {
   for (var i = 0; i < allProducts.length; i++){
     displayedChart[i] =  productArray[i].timesDisplayed;
   }
 };
-
-// MAKE DATA ARRAY FOR CLICKED ITEMS
 var clickedChart  = [];
 function dataSets3() {
   for (var i = 0; i < allProducts.length; i++){
     clickedChart[i] =  productArray[i].timesClicked;
+  }
 };
-
 // MAKE PERCENT CHART
 function chart2() {
+  var nameChart2 = document.getElementById('nameChart2');
+  nameChart2.textContent = 'Times Products Were Clicked vs Displayed';
+  var title2 = document.getElementById("title2");
+  var canvasTwo = document.getElementById("canvasTwo");
+  var throwawayNode = title2.removeChild(canvasTwo);
+  var canvasTwo = document.createElement('canvas');
+  canvasTwo.id = 'canvasTwo';
+  canvasTwo.setAttribute('width', '400');
+  canvasTwo.setAttribute('height', '400');
+  title2.appendChild(canvasTwo);
   var data = {
-    labels: label,
+    labels: ['Bag', 'Banana', 'Boots', 'Chair', 'Cthulhu', 'Dragon', 'Pen', 'Scissors', 'Shark', 'Sweep', 'Unicorn', 'USB', 'Water Can', 'Wine Glass'],
     datasets: [
       {
-      label: 'Displayed Chart',
-      fillColor: "rgba(202,205,206,0.5)",
-      strokeColor: "rgba(202,205,206,0.8)",
-      highlightFill: "rgba(202,205,206,0.75)",
-      highlightStroke: "rgba(202,205,206,1)",
-      data: displayedChart
+        label: 'Times Clicked',
+        fillColor: "#E6E6FA",
+        strokeColor: "#E6E6FA",
+        highlightFill: "rgba(151,187,205,0.75)",
+        highlightStroke: "rgba(151,187,205,1)",
+        scaleFontColor: "#DCDCDC",
+        data: clickedChart
       },
       {
-      label: 'Clicked Chart',
-      fillColor: "rgba(202,205,206,0.5)",
-      strokeColor: "rgba(202,205,206,0.8)",
-      highlightFill: "rgba(202,205,206,0.75)",
-      highlightStroke: "rgba(202,205,206,1)",
-      data: clickedChart
+        label: 'Times Displayed',
+        fillColor: "#FFE4B5",
+        strokeColor: "#FFE4B5",
+        highlightFill: "rgba(220,220,220,0.75)",
+        highlightStroke: "rgba(220,220,220,1)",
+        scaleFontColor: "#DCDCDC",
+        data: displayedChart
       }
     ]
   };
-  // MAKE CHARTS THAT SHOWS CLICKED VS DISPLAYED DATA
   var ctx = document.getElementById('canvasTwo').getContext('2d');
-  var myBarChart = new Chart(ctx).Bar(data);
+  var myBarChart2 = new Chart(ctx).Bar(data);
+  document.getElementById('legendTwo').innerHTML = myBarChart2.generateLegend();
 }
